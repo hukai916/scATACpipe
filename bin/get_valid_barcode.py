c8b8a026914596a18bb84b33e22bdfab98147012
Kai Hu <>
2021-11-25 16:48:05 -0500

    minor

--- bin/get_valid_barcode.py
+++ bin/get_valid_barcode.py
@@ -14,15 +14,29 @@ whitelist_barcode       = sys.argv[2]
 outfile_name            = sys.argv[3]
 
 dict_valid_barcode = {}
-with open(valid_barcode_frequency) as f:
-    for line in f:
-        tem = line.split()
-    if not tem[0] in dict_valid_barcode:
-        dict_valid_barcode[tem[0]] = tem[1]
+if valid_barcode_frequency.endswith(".gz"):
+    with gzip.open(valid_barcode_frequency, "rt") as f:
+        for line in f:
+            tem = line.split()
+        if not tem[0] in dict_valid_barcode:
+            dict_valid_barcode[tem[0]] = tem[1]
+else:
+    with open(valid_barcode_frequency) as f:
+        for line in f:
+            tem = line.split()
+        if not tem[0] in dict_valid_barcode:
+            dict_valid_barcode[tem[0]] = tem[1]
 
 with open(outfile_name, "w") as fout:
-    with open(whitelist_barcode) as fin:
-        for line in fin:
-            tem = line.split()[0]
-            if tem in dict_valid_barcode:
-                fout.write(tem + "\n")
+    if whitelist_barcode.endswith(".gz"):
+        with gzip.open(whitelist_barcode, "rt") as fin:
+            for line in fin:
+                tem = line.split()[0]
+                if tem in dict_valid_barcode:
+                    fout.write(tem + "\n")
+    else:
+        with open(whitelist_barcode) as fin:
+            for line in fin:
+                tem = line.split()[0]
+                if tem in dict_valid_barcode:
+                    fout.write(tem + "\n")
