d7cfca071271acb9ae583ad50730c9b15988056e
Kai Hu <>
2021-12-09 17:10:39 -0500

    minor

--- bin/remove_duplicate.py
+++ bin/remove_duplicate.py
@@ -53,7 +53,11 @@ def rm_dup(intervals, inbam, header_len_dict,
     total_unique_fragment_num    = 0
     total_duplicate_fragment_num = 0
 
-    inbam   = pysam.AlignmentFile(inbam, "rb")
+    try:
+        inbam   = pysam.AlignmentFile(inbam, "rb")
+    except:
+        pysam.index(inbam)
+        inbam   = pysam.AlignmentFile(inbam, "rb")
     outbam  = pysam.AlignmentFile(outname, "wb", template = inbam)
 
     # fill in the read_dict dictionary:
