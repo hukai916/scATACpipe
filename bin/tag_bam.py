d4d3f82186be73f268ae69b35429980e93e4d8d3
Kai Hu <>
2021-12-24 13:19:15 -0500

    minor

--- bin/tag_bam.py
+++ bin/tag_bam.py
@@ -52,7 +52,7 @@ def set_tag(intervals, inbam, dict_tag, tag):
     prefix  = re.sub(".bam$", "", os.path.basename(bam))
     outname = os.path.join("tagged_" + prefix + "_chunk_" + temp_name + ".bam")
     outbam  = pysam.AlignmentFile(outname, "wb", template = inbam)
-    print(outbam)
+    # print(outbam)
 
     for i in intervals:
         for read in inbam.fetch(i[0], i[1], i[2]):
