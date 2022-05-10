76077361a55f4f06eeaf6d7c6ff5302271781e37
Kai Hu <>
2022-05-10 15:46:18 -0400

    minor

--- bin/remove_duplicate.py
+++ bin/remove_duplicate.py
@@ -93,13 +93,15 @@ def rm_dup(intervals, inbam, header_len_dict,
                         left_read_check = 1
                         match_5   = re.search(soft_clip_5, cigar)
                         match_3   = re.search(soft_clip_3, cigar)
-                        if len(match_5.groups()) > 0:
-                            left_read.reference_start = left_read.reference_start - int(match_5.group(1)) + shift_forward
-                        else:
+                        if isinstance(match_5, type(None)):
                             left_read.reference_start = left_read.reference_start + shift_forward
-                        if len(match_3.groups()) > 0:
+                        else:
+                            left_read.reference_start = left_read.reference_start - int(match_5.group(1)) + shift_forward
+
+                        if not isinstance(match_3, type(None)):
                             left_read_reference_end = left_read.reference_end + int(match_3.group(1))
-                        if len(match_5.groups()) or len(match_3.groups()):
+
+                        if not (isinstance(match_3, type(None)) and isinstance(match_5, type(None))):
                             soft_clip_num += 1
 
                         # ensure reads are within boundaries:
@@ -115,15 +117,17 @@ def rm_dup(intervals, inbam, header_len_dict,
                         right_read_check = 1
                         match_3    = re.search(soft_clip_3, cigar)
                         match_5    = re.search(soft_clip_5, cigar)
-                        if len(match_3.groups()) > 0:
-                            # frag_end_pos = right_read.reference_end + int(match_3.group(1)) + shift_reverse
-                            right_read_reference_end = right_read.reference_end + int(match_3.group(1)) + shift_reverse
-                        else:
+                        if isinstance(match_3, type(None)):
                             # frag_end_pos = right_read.reference_end + shift_reverse
                             right_read_reference_end = right_read.reference_end + shift_reverse
-                        if len(match_5.groups()) > 0:
+                        else:
+                            # frag_end_pos = right_read.reference_end + int(match_3.group(1)) + shift_reverse
+                            right_read_reference_end = right_read.reference_end + int(match_3.group(1)) + shift_reverse
+
+                        if not isinstance(match_5, type(None)):
                             right_read.reference_start = right_read.reference_start - int(match_5.group(1))
-                        if len(match_5.groups()) or len(match_3.groups()):
+
+                        if not (isinstance(match_5, type(None)) and isinstance(match_5, type(None))):
                             soft_clip_num += 1
 
                         # ensure reads are within boundaries:
